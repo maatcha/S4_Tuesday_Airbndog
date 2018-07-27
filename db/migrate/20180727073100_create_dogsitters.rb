7ce3d79e1d95cc6138ebad5184e4bb2439f61a8f
maatcha <mattcha54@gmail.com>
2018-07-27 09:36:16 +0200

    structure_ok

---
+++ db/migrate/20180727073100_create_dogsitters.rb
@@ -0,0 +1,9 @@
+class CreateDogsitters < ActiveRecord::Migration[5.2]
+  def change
+        create_table :strolls do |t|
+      t.belongs_to :dog, index:true
+      t.belongs_to :dogsitter, index:true
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+num = 10
+
+num.times do 
+	a = City.create(name: Faker::Address.city)
+	b = Dog.create(name: Faker::Dog.name, breed:Faker::Dog.breed, city_id: a.id)
+	c = Dogsitter.create(first_name: Faker::Name.last_name, last_name: Faker::Name.last_name, city_id:a.id)
+	b.dogsitters << c
+	c.dogs << b
+	
+end
